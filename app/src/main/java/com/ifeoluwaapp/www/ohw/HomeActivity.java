0ce8ff88c981d41cd795ac3ad490de3a4509b584
PaimoEmmanuel <paimoemmanuel2016@gmail.com>
2019-08-01 14:42:50 +0100

    Work in progress. Splash, Sign up and home activities.

---
+++ app/src/main/java/com/ifeoluwaapp/www/ohw/HomeActivity.java
@@ -0,0 +1,47 @@
+package com.ifeoluwaapp.www.ohw;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_categories:
+                    mTextMessage.setText(R.string.categories);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.Account);
+                    return true;
+                case R.id.navigation_support:
+                    mTextMessage.setText(R.string.support);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
